4336ead1b7f021f290bf404a473c5a8edb913c25
David Sferruzza <david.sferruzza@gmail.com>
2020-04-08 15:35:53 +0200

    Fix blower & servos handling

--- src/software/firmware/srcs/respirator.cpp
+++ src/software/firmware/srcs/respirator.cpp
@@ -105,26 +105,26 @@ void setup() {
     alarmController = AlarmController();
 #elif HARDWARE_VERSION == 2
     // Timer for servos
-    hardwareTimer1 = new HardwareTimer(TIM1);
-    hardwareTimer1->setOverflow(SERVO_VALVE_PERIOD, MICROSEC_FORMAT);
+    hardwareTimer3 = new HardwareTimer(TIM3);
+    hardwareTimer3->setOverflow(SERVO_VALVE_PERIOD, MICROSEC_FORMAT);
 
     // Servo blower setup
-    servoBlower = PressureValve(hardwareTimer1, TIM_CHANNEL_SERVO_VALVE_BLOWER, PIN_SERVO_BLOWER,
+    servoBlower = PressureValve(hardwareTimer3, TIM_CHANNEL_SERVO_VALVE_BLOWER, PIN_SERVO_BLOWER,
                                 VALVE_OPEN_STATE, VALVE_CLOSED_STATE);
     servoBlower.setup();
-    hardwareTimer1->resume();
+    hardwareTimer3->resume();
 
     // Servo patient setup
     servoPatient = PressureValve(hardwareTimer3, TIM_CHANNEL_SERVO_VALVE_PATIENT, PIN_SERVO_PATIENT,
                                  VALVE_OPEN_STATE, VALVE_CLOSED_STATE);
     servoPatient.setup();
-    hardwareTimer1->resume();
+    hardwareTimer3->resume();
 
     // Timer for escBlower
-    hardwareTimer3 = new HardwareTimer(TIM3);
-    hardwareTimer3->setOverflow(SERVO_VALVE_PERIOD, MICROSEC_FORMAT);
+    hardwareTimer1 = new HardwareTimer(TIM1);
+    hardwareTimer1->setOverflow(SERVO_VALVE_PERIOD, MICROSEC_FORMAT);
 
-    blower = Blower(hardwareTimer3, TIM_CHANNEL_ESC_BLOWER, PIN_ESC_BLOWER);
+    blower = Blower(hardwareTimer1, TIM_CHANNEL_ESC_BLOWER, PIN_ESC_BLOWER);
     blower.setup();
     blower_pointer = &blower;
 
